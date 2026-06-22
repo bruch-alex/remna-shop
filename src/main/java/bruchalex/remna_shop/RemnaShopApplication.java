d9e57104cf0414c36731fd3062fbe445cbc57751
Alex Bruch <alex.bruch@tutanota.com>
2026-06-22 18:46:45 +0200

    init

---
+++ src/main/java/bruchalex/remna_shop/RemnaShopApplication.java
@@ -0,0 +1,13 @@
+package bruchalex.remna_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemnaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemnaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bruchalex/remna_shop/RemnaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package bruchalex.remna_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemnaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
