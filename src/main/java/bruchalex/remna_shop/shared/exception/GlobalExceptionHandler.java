792a2c64da1889c8d9b82c650ff1d186a9c977e1
Alex Bruch <alex.bruch@tutanota.com>
2026-07-08 17:29:12 +0200

    feat: handle http method not allowed exception

--- src/main/java/bruchalex/remna_shop/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/bruchalex/remna_shop/shared/exception/GlobalExceptionHandler.java
@@ -3,14 +3,18 @@ package bruchalex.remna_shop.shared.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Slf4j
@@ -42,5 +46,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .badRequest()
                 .body(response);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+
+        HttpHeaders headers = new HttpHeaders();
+        if (ex.getSupportedHttpMethods() != null) {
+            headers.setAllow(new HashSet<>(ex.getSupportedHttpMethods()));
+        }
+
+        var body = new ErrorResponse("Method not allowed");
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .headers(headers)
+                .body(body);
     }
 }
