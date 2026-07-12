8cdd81b55db0f8d71f01674a0b8440e95fc7e920
Alex Bruch <alex.bruch@tutanota.com>
2026-07-12 21:34:50 +0200

    feat: add test data to tariffs

---
+++ src/main/java/bruchalex/remna_shop/tariff/infra/bootstrap/DevTariffSeeder.java
@@ -0,0 +1,51 @@
+package bruchalex.remna_shop.tariff.infra.bootstrap;
+
+
+import bruchalex.remna_shop.tariff.application.CreateNewTariffCommand;
+import bruchalex.remna_shop.tariff.application.CreateNewTariffUseCase;
+import bruchalex.remna_shop.tariff.application.GetAllTariffsUseCase;
+import bruchalex.remna_shop.tariff.application.SetNewTrialTariffUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class DevTariffSeeder implements CommandLineRunner {
+
+    private final CreateNewTariffUseCase createNewTariffUseCase;
+    private final SetNewTrialTariffUseCase setNewTrialTariffUseCase;
+    private final GetAllTariffsUseCase getAllTariffsUseCase;
+
+    @Override
+    public void run(String... args) throws Exception {
+        var command = new CreateNewTariffCommand(
+                "Test Tariff",
+                100,
+                10,
+                30,
+                500
+        );
+
+        var commandTrial = new CreateNewTariffCommand(
+                "Trial Tariff",
+                69,
+                3,
+                7,
+                100
+        );
+        createNewTariffUseCase.execute(command);
+        createNewTariffUseCase.execute(commandTrial);
+
+        var tariffs = getAllTariffsUseCase.execute(false);
+
+        for (var tariff : tariffs) {
+            if (tariff.name().equals("Trial Tariff")) {
+                setNewTrialTariffUseCase.execute(tariff.id());
+                break;
+            }
+        }
+    }
+}
