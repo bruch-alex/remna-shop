6f6a58508acd2f010d0d406bdbe1ac2ceef501eb
Alex Bruch <alex.bruch@tutanota.com>
2026-07-24 20:13:13 +0200

    refactor(vpn): rename vpn schema and table

--- src/main/java/bruchalex/remna_shop/vpn/domain/Profile.java
+++ src/main/java/bruchalex/remna_shop/vpn/domain/Profile.java
@@ -1,17 +1,26 @@
 package bruchalex.remna_shop.vpn.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
+@Entity
+@Table(name = "profiles", schema = "vpn_module")
 public class Profile {
+
+    @Id
     private UUID uuid;
     private UUID userUuid;
 
